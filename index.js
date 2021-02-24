24ef79b60fbeb0f012844335108202020e7b4798
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2021-02-24 18:24:59 +0100

    start1
    wow

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+// 1. Создать функцию, которая возвращает true/false, на вход принимает обьект,
+//в котором есть поле age, и если age > 18 ? true : false
+
+//const user = { age: 21 }
+
+
+const user = {
+   age: 21,
+};
+
+tiketToTheClub(age) {
+  if (age >18){
+   return true;
+  } else {
+   return false;
+  }
+}
+
+
+
+//    tiketToTheClub(age) {
+//    return age > 18 ? true : false ;
+//    }
