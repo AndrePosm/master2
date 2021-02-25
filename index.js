5e2785eeb7e228767f037eebf55c756e030d93c0
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2021-02-25 20:33:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,13 @@
 // 1. Создать функцию, которая возвращает true/false, на вход принимает обьект,
 //в котором есть поле age, и если age > 18 ? true : false
 
-//const user = { age: 21 }
+// const user = { age: 21, name: 'Petya' }
 
+    const user = {age: 22, name: 'Petya'};
 
-const user = {
-   age: 21,
-};
-
-tiketToTheClub(age) {
-  if (age >18){
-   return true;
-  } else {
-   return false;
+    function canGoIn (user){
+    return user.age > 18;
   }
-}
 
 
-// Just test
-//    tiketToTheClub(age) {
-//    return age > 18 ? true : false ;
-//    }
+console.log(canGoIn);
